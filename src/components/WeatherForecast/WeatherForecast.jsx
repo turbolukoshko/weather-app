e3dddfeb58a355abf9bb4fa790f53a51993849b9
turbolukoshko <turbolukoshko@gmail.com>
2020-11-13 00:13:33 +0200

    added WeatherForecastComponent and styles

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Weather from "../Weather/Weather";
+
+const WeatherForecast = () => {
+  return (
+    <div>
+      <h2>Todays weather</h2>
+      <ul>
+        <li>
+          <p>3 am</p>
+          <img src="" alt="" />
+          <p>10 c</p>
+        </li>
+        <li>
+          <p>3 am</p>
+          <img src="" alt="" />
+          <p>10 c</p>
+        </li>
+        <li>
+          <p>3 am</p>
+          <img src="" alt="" />
+          <p>10 c</p>
+        </li>
+        <li>
+          <p>3 am</p>
+          <img src="" alt="" />
+          <p>10 c</p>
+        </li>
+        <li>
+          <p>3 am</p>
+          <img src="" alt="" />
+          <p>10 c</p>
+        </li>
+        <li>
+          <p>3 am</p>
+          <img src="" alt="" />
+          <p>10 c</p>
+        </li>
+        <li>
+          <p>3 am</p>
+          <img src="" alt="" />
+          <p>10 c</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default WeatherForecast;
