6ad13bbc591707510eedfa4eafd994ae5491aeab
turbolukoshko <turbolukoshko@gmail.com>
2020-11-13 23:30:41 +0200

    added styles for WeatherForecast component

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,42 +1,42 @@
 import React from "react";
-import Weather from "../Weather/Weather";
+import './WeatherForecast.scss';
 
 const WeatherForecast = () => {
   return (
-    <div>
-      <h2>Todays weather</h2>
-      <ul>
-        <li>
+    <div className="weather-forecast">
+      <h2 className="weather-forecast__title">Todays weather</h2>
+      <ul className="weather-forecast__list">
+        <li className="weather-forecast__item">
           <p>3 am</p>
           <img src="" alt="" />
           <p>10 c</p>
         </li>
-        <li>
+        <li className="weather-forecast__item">
           <p>3 am</p>
           <img src="" alt="" />
           <p>10 c</p>
         </li>
-        <li>
+        <li className="weather-forecast__item">
           <p>3 am</p>
           <img src="" alt="" />
           <p>10 c</p>
         </li>
-        <li>
+        <li className="weather-forecast__item">
           <p>3 am</p>
           <img src="" alt="" />
           <p>10 c</p>
         </li>
-        <li>
+        <li className="weather-forecast__item">
           <p>3 am</p>
           <img src="" alt="" />
           <p>10 c</p>
         </li>
-        <li>
+        <li className="weather-forecast__item">
           <p>3 am</p>
           <img src="" alt="" />
           <p>10 c</p>
         </li>
-        <li>
+        <li className="weather-forecast__item">
           <p>3 am</p>
           <img src="" alt="" />
           <p>10 c</p>
