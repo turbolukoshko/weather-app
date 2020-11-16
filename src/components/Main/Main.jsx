d67f8865af9ca259ea6bb94daabe23d0a9b2ff5d
turbolukoshko <turbolukoshko@gmail.com>
2020-11-16 19:04:08 +0200

    added preloader in app

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,38 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Weather from '../Weather/Weather';
-import WeatherForecast from '../WeatherForecast/WeatherForecast';
-import {weatherAction} from '../../store/actions/weatherAction.js'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import Weather from "../Weather/Weather";
+import WeatherForecast from "../WeatherForecast/WeatherForecast";
+import { weatherAction } from "../../store/actions/weatherAction.js";
+import Loader from "../UI/Loader/Loader";
 
 class Main extends Component {
-
   async componentDidMount() {
     this.props.getWeather();
   }
 
   render() {
-    console.log(this.props)
-    return(
+    console.log(this.props);
+    return (
       <main className="main">
-        <Weather weather={this.props.data}/>
-        <WeatherForecast/>
+        {this.props.loading ? (
+          <Loader />
+        ) : (
+          <Fragment>
+            <Weather weather={this.props.data} />
+            <WeatherForecast />
+          </Fragment>
+        )}
       </main>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     loading: state.weatherReducer.loading,
     data: state.weatherReducer.data,
     errors: state.weatherReducer.errors,
-  }
+  };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     getWeather: () => dispatch(weatherAction()),
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
---
+++ src/components/UI/Loader/Loader.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Loader.scss';
+
+const Loader = () => {
+  return (
+    <div class="loadingio-spinner-spinner-5t8gqjsqcq loader">
+      <div class="ldio-i5vwdk4cfx">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
