facc9a07df1f3cf3ee30b676d9f82d95ea63a88e
turbolukoshko <turbolukoshko@gmail.com>
2020-11-24 23:59:53 +0200

    added method for search weather

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,21 +7,67 @@ import Loader from "../UI/Loader/Loader";
 import './Main.scss';
 
 class Main extends Component {
-  async componentDidMount() {
-    this.props.getWeather();
+
+  state = {
+    city: '',
+    geolocation: {
+      latitude: null,
+      longitude: null,
+    },
+  }
+
+  componentDidMount() {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    
+    const success = (pos) => {
+      const crd = pos.coords;
+
+      this.setState((prevState) => ({
+        ...prevState.geolocation,
+        geolocation: {
+          latitude: crd.latitude,
+          longitude: crd.longitude,
+        }
+      }))
+    };
+    
+    const error = (err) => {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    };
+    
+    navigator.geolocation.getCurrentPosition(success, error, options);
+    // this.props.getWeather(this.state.geolocation);
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.geolocation !== this.state.geolocation) {
+      this.props.getWeather(this.state.geolocation, null);
+    }
+  }
+
+  onChangeInput = event => {
+    this.setState({city: event.target.value});
+  }
+
+  findWeather = event => {
+    event.preventDefault();
+    this.props.getWeather(this.state.geolocation, this.state.city);
   }
 
   render() {
     return (
       <main className="main">
-        {this.props.loading ? (
-          <Loader />
-        ) : (
+        {this.props.loading && !this.props.errors && <Loader />}
+        {!this.props.loading && !this.props.errors &&
           <Fragment>
-            <Weather weather={this.props.currentWeather} />
+            <Weather weather={this.props.currentWeather} onChangeInput={this.onChangeInput} findWeather={(e) => this.findWeather(e)}/>
             <WeatherForecast forecast={this.props.forecast}/>
           </Fragment>
-        )}
+        }
       </main>
     );
   }
@@ -38,7 +84,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getWeather: () => dispatch(weatherAction()),
+    getWeather: (geolocation, city) => dispatch(weatherAction(geolocation, city)),
   };
 };
 
