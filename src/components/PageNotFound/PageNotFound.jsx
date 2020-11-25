a18d9085ff41443128e0144543851e889d3f7b81
turbolukoshko <turbolukoshko@gmail.com>
2020-11-26 00:48:29 +0200

    added the page not found component

---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './PageNotFound.scss';
+
+const PageNotFound = props => {
+  return (
+    props.error && (
+      <div className="weather-not-found">
+        <h1 className="weather-not-found__title">Oh no! Something went wrong.</h1>
+        <p className="weather-not-found__description">We couldn't find the city. Please, check the entry is correct and try again.</p>
+        <div className="buttons-panel">
+          <a href="/" className="weather-not-found__link">Go home</a>
+        </div>
+      </div>
+    )
+  );
+}
+
+export default PageNotFound;
