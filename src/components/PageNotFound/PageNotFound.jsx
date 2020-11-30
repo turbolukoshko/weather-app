7498b1f0d4e7e371a0cdcbfa8f6b450830aab25a
turbolukoshko <turbolukoshko@gmail.com>
2020-12-01 00:34:24 +0200

    fixed the PageNotFound component and styles

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -5,6 +5,7 @@ const PageNotFound = props => {
   return (
     props.error && (
       <div className="weather-not-found">
+        <p className="weather-code">404</p>
         <h1 className="weather-not-found__title">Oh no! Something went wrong.</h1>
         <p className="weather-not-found__description">We couldn't find the city. Please, check the entry is correct and try again.</p>
         <div className="buttons-panel">
